0d0244f928261ce1e491e9a7d90a26eca29dfd9c
Rayancoder21 <rayan.h.khogali@gmail.com>
2024-05-22 02:33:35 -0700

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""For a given employee ID, returns information about
-their TODO list progress"""
+"""Exports to-do list information for a given employee ID to CSV format"""
 import csv
 import requests
 import sys
@@ -12,8 +11,7 @@ if __name__ == "__main__":
     user_id = sys.argv[1]
     user = requests.get(url + "users/{}".format(user_id)).json()
     username = user.get("username")
-    params = {"userId": user_id}
-    todos = requests.get(url + "todos", params).json()
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
     with open("{}.csv".format(user_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         [writer.writerow(
